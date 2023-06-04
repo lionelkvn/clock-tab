b90bec055597fedced7ee62c251cf31eeb6ab0e9
lionelkvn <lionel.natividad@gmail.com>
2023-06-04 09:54:15 +0800

    modified Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ function Navbar () {
              
                 <ul className='container col-4 d-flex p-2 justify-content-evenly list-inline'>
                
-                    <li className='list-inline item'>Clocktab</li>
-                    <li>Countdown</li>
+                    <li className='list-inline-item'>Clocktab</li>
+                    <li className='l'>Countdown</li>
                     <li>Timer Tab</li>
                     <li>Msg Tab</li>
                 
